e21c501ad04d8d027a5c75e34fffd9eaa166c5aa
Nathaniel Parah <nathanielparah579@gmail.com>
2026-03-16 01:16:54 +0100

    created a logic where flags will be used to represent files being read for hashing
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/get_input.rs
+++ src/get_input.rs
@@ -1,6 +1,8 @@
 use std::{
     env,
     error::Error,
+    fs::*,
+    path::PathBuf,
 };
 
 pub fn get_args() -> Result<String, Box<dyn Error>> {
@@ -10,7 +12,15 @@ pub fn get_args() -> Result<String, Box<dyn Error>> {
         return Err(format!("No arguents were provided").into());
     }
 
-    let user_input = args.get(1).ok_or("index out of bounds")?.to_string();
+    let user_input = args.get(1).ok_or("index out of bounds")?;
 
-    Ok(user_input)
+    let flag_arg = args.get(2).ok_or("invalid flag")?;
+
+    let file_input = PathBuf::from(user_input);
+
+    if flag_arg == "-f" && user_input != "" {
+        return Ok(read_to_string(&file_input)?);
+    }
+    
+    Ok(user_input.to_string())
 }
