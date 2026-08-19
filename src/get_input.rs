90fa54252a638e9a6d6eace8eeec881f725dd91e
Nathaniel Parah <nathanielparah579@gmail.com>
2026-08-19 22:53:20 +0100

    fix: removed unwanted line of code
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/get_input.rs
+++ src/get_input.rs
@@ -5,6 +5,12 @@ use std::{
     path::PathBuf,
 };
 
+//so we use the environment variable to accept arguments from the command line, 
+//then we store them in a list which we use a vector, and then check if the argument
+//is less than two, cause the name of the program stands as the first argument in the 
+//list, and then what ever you put serves as the second argument, so this tool checks 
+//if no argument was provided, and if not, then it returns and error message and panics
+//so here, we want  
 pub fn get_args() -> Result<String, Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
